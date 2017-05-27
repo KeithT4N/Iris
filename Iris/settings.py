78e3c084afdad052b123742e64c607000047f0de
Keith Tan <samkeithtan@me.com>
2017-05-27 19:30:33 +0800

    Fixed run error

--- Iris/settings.py
+++ Iris/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFpytrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'Iris.urls'
